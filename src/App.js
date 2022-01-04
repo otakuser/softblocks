ce2f0f836033b1635bcc50f29aa33f325ab6d87d
alex <you@example.com>
2022-01-05 00:57:57 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deployment is hapenning
+          Probando cambios
         </p>
         <a
           className="App-link"
